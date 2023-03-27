fd52c90aa20b72a008591641c1c77ebeb1a3a73e
Caleb Bellisle <bellisc554@lynchburg.edu>
2023-03-27 11:11:31 -0400

    updated gamme files

--- Chapter09/Game.cpp
+++ Chapter09/Game.cpp
@@ -21,11 +21,19 @@
 #include "FollowActor.h"
 #include "OrbitActor.h"
 #include "SplineActor.h"
-
+#include "UIScreen.h" //CB
+#include "Font.h"//CB
+#include "PauseMenu.h"//CB
+#include "StartMenu.h"//CB
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+#include <fstream>
+#include <sstream>
+#include <rapidjson/document.h>
 Game::Game()
 :mRenderer(nullptr)
 ,mAudioSystem(nullptr)
-,mIsRunning(true)
+,mGameState(EGameplay)
 ,mUpdatingActors(false)
 {
 	
@@ -60,8 +68,13 @@ bool Game::Initialize()
 		return false;
 	}
 
-	LoadData();
+	if (TTF_Init() != 0)
+	{
+		SDL_Log("Failed to initialize SDL_TTF");
+		return false;
+	}
 
+	LoadData();
 	mTicksCount = SDL_GetTicks();
 	
 	return true;
@@ -69,8 +82,9 @@ bool Game::Initialize()
 
 void Game::RunLoop()
 {
-	while (mIsRunning)
+	while (mGameState != EQuit)
 	{
+
 		ProcessInput();
 		UpdateGame();
 		GenerateOutput();
@@ -84,28 +98,54 @@ void Game::ProcessInput()
 	{
 		switch (event.type)
 		{
-			case SDL_QUIT:
-				mIsRunning = false;
-				break;
+		case SDL_QUIT:
+			mGameState = EQuit;
+			break;
 			// This fires when a key's initially pressed
-			case SDL_KEYDOWN:
-				if (!event.key.repeat)
+		case SDL_KEYDOWN:
+			if (!event.key.repeat)
+			{
+				if (mGameState == EGameplay)
 				{
 					HandleKeyPress(event.key.keysym.sym);
 				}
-				break;
-			case SDL_MOUSEBUTTONDOWN:
+				else if (!mUIStack.empty())
+				{
+					mUIStack.back()->
+						HandleKeyPress(event.key.keysym.sym);
+				}
+			}
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			if (mGameState == EGameplay)
+			{
 				HandleKeyPress(event.button.button);
-				break;
-			default:
-				break;
+			}
+			else if (!mUIStack.empty())
+			{
+				mUIStack.back()->
+					HandleKeyPress(event.button.button);
+			}
+			break;
+		default:
+			break;
 		}
 	}
-	
+
 	const Uint8* state = SDL_GetKeyboardState(NULL);
-	if (state[SDL_SCANCODE_ESCAPE])
+	if (mGameState == EGameplay)
 	{
-		mIsRunning = false;
+		for (auto actor : mActors)
+		{
+			if (actor->GetState() == Actor::EActive)
+			{
+				actor->ProcessInput(state);
+			}
+		}
+	}
+	else if (!mUIStack.empty())
+	{
+		mUIStack.back()->ProcessInput(state);
 	}
 
 	for (auto actor : mActors)
@@ -118,6 +158,11 @@ void Game::HandleKeyPress(int key)
 {
 	switch (key)
 	{
+	case SDLK_ESCAPE:
+		// Create pause menu
+		//new PauseMenu(this);
+		new PauseMenu(this);
+		break;
 	case '-':
 	{
 		// Reduce master volume
@@ -168,38 +213,63 @@ void Game::UpdateGame()
 	}
 	mTicksCount = SDL_GetTicks();
 
-	// Update all actors
-	mUpdatingActors = true;
-	for (auto actor : mActors)
+	if (mGameState == EGameplay)
 	{
-		actor->Update(deltaTime);
-	}
-	mUpdatingActors = false;
-
-	// Move any pending actors to mActors
-	for (auto pending : mPendingActors)
-	{
-		pending->ComputeWorldTransform();
-		mActors.emplace_back(pending);
-	}
-	mPendingActors.clear();
-
-	// Add any dead actors to a temp vector
-	std::vector<Actor*> deadActors;
-	for (auto actor : mActors)
-	{
-		if (actor->GetState() == Actor::EDead)
+		// Update all actors
+		mUpdatingActors = true;
+		for (auto actor : mActors)
 		{
-			deadActors.emplace_back(actor);
+			actor->Update(deltaTime);
+		}
+		mUpdatingActors = false;
+
+		// Move any pending actors to mActors
+		for (auto pending : mPendingActors)
+		{
+			pending->ComputeWorldTransform();
+			mActors.emplace_back(pending);
+		}
+		mPendingActors.clear();
+
+		// Add any dead actors to a temp vector
+		std::vector<Actor*> deadActors;
+		for (auto actor : mActors)
+		{
+			if (actor->GetState() == Actor::EDead)
+			{
+				deadActors.emplace_back(actor);
+			}
+		}
+		// Delete dead actors (which removes them from mActors)
+		for (auto actor : deadActors)
+		{
+			delete actor;
 		}
 	}
 
-	// Delete dead actors (which removes them from mActors)
-	for (auto actor : deadActors)
-	{
-		delete actor;
-	}
 
+	// Update UI screens
+	for (auto ui : mUIStack)
+	{
+		if (ui->GetState() == UIScreen::EActive)
+		{
+			ui->Update(deltaTime);
+		}
+	}
+	// Delete any UIScreens that are closed
+	auto iter = mUIStack.begin();
+	while (iter != mUIStack.end())
+	{
+		if ((*iter)->GetState() == UIScreen::EClosing)
+		{
+			delete* iter;
+			iter = mUIStack.erase(iter);
+		}
+		else
+		{
+			++iter;
+		}
+	} 
 	// Update audio system
 	mAudioSystem->Update(deltaTime);
 
@@ -215,6 +285,9 @@ void Game::GenerateOutput()
 
 void Game::LoadData()
 {
+
+	LoadText("Assets/English.gptext");
+
 	// Create actors
 	Actor* a = new Actor(this);
 	a->SetPosition(Vector3(200.0f, 75.0f, 0.0f));
@@ -348,6 +421,11 @@ void Game::UnloadData()
 	{
 		delete mActors.back();
 	}
+	while (!mUIStack.empty())
+	{
+		delete mUIStack.back();
+		mUIStack.pop_back();
+	}
 
 	if (mRenderer)
 	{
@@ -400,5 +478,86 @@ void Game::RemoveActor(Actor* actor)
 		// Swap to end of vector and pop off (avoid erase copies)
 		std::iter_swap(iter, mActors.end() - 1);
 		mActors.pop_back();
+	}
+}
+
+
+void Game::PushUI(UIScreen* screen)
+{
+	mUIStack.emplace_back(screen);
+}
+Font* Game::GetFont(const std::string& fileName)
+{
+	auto iter = mFonts.find(fileName);
+	if (iter != mFonts.end())
+	{
+		return iter->second;
+	}
+	else
+	{
+		Font* font = new Font(this);
+		if (font->Load(fileName))
+		{
+			mFonts.emplace(fileName, font);
+		}
+		else
+		{
+			font->Unload();
+			delete font;
+			font = nullptr;
+		}
+		return font;
+	}
+}
+
+void Game::LoadText(const std::string& fileName)
+{
+	// Clear the existing map, if already loaded
+	mText.clear();
+	// Try to open the file
+	std::ifstream file(fileName);
+	if (!file.is_open())
+	{
+		SDL_Log("Text file %s not found", fileName.c_str());
+		return;
+	}
+	// Read the entire file to a string stream
+	std::stringstream fileStream;
+	fileStream << file.rdbuf();
+	std::string contents = fileStream.str();
+	// Open this file in rapidJSON
+	rapidjson::StringStream jsonStr(contents.c_str());
+	rapidjson::Document doc;
+	doc.ParseStream(jsonStr);
+	if (!doc.IsObject())
+	{
+		SDL_Log("Text file %s is not valid JSON", fileName.c_str());
+		return;
+	}
+	// Parse the text map
+	const rapidjson::Value& actions = doc["TextMap"];
+	for (rapidjson::Value::ConstMemberIterator itr = actions.MemberBegin();
+		itr != actions.MemberEnd(); ++itr)
+	{
+		if (itr->name.IsString() && itr->value.IsString())
+		{
+			mText.emplace(itr->name.GetString(),
+				itr->value.GetString());
+		}
+	}
+}
+
+const std::string& Game::GetText(const std::string& key)
+{
+	static std::string errorMsg("**KEY NOT FOUND**");
+	// Find this text in the map, if it exists
+	auto iter = mText.find(key);
+	if (iter != mText.end())
+	{
+		return iter->second;
+	}
+	else
+	{
+		return errorMsg;
 	}
 }
--- Chapter09/Texture.h
+++ Chapter09/Texture.h
@@ -13,12 +13,13 @@ class Texture
 public:
 	Texture();
 	~Texture();
-	
+
 	bool Load(const std::string& fileName);
 	void Unload();
-	
+	void CreateFromSurface(struct SDL_Surface* surface);
+
 	void SetActive();
-	
+
 	int GetWidth() const { return mWidth; }
 	int GetHeight() const { return mHeight; }
 private:
